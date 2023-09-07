3d4e6b58bcefc3041e363c997b21d16de0318786
user.walid1921 <user.kouiderayadwalid@gmail.com>
2023-09-07 20:52:33 +0300

    new file

---
+++ Tailwind React Starter/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+  return (
+    <header>
+     <nav>
+      <a href="/">
+        {/* <img src="" alt="" /> */}
+        Navbar
+      </a>
+     </nav>
+    </header>
+  );
+};
+
+export default Navbar;
